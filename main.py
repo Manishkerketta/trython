2d937edf5f6b5d1f19e1cc561b3c52160b3b3150
mastermontu <manishkerketta22@gmail.com>
2025-10-31 23:53:15 +0530

    remove cors

--- main.py
+++ main.py
@@ -2,22 +2,11 @@ import uvicorn
 from fastapi import FastAPI
 from instrument_api import router as instrument_router
 from vwap_api import router as vwap_router
-from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 
 app.include_router(instrument_router)
 app.include_router(vwap_router)
-origins = [
-    "http://localhost:3000",   # your frontend origin
-    # Add more origins if needed
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
